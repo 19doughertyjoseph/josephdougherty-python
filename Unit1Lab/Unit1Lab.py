53db5793fb162d0a4d4a966b6c5a1b16421a4286
Gabi Hyman <19hymangabrielle@bprep.org>
2018-09-07 09:16:53 -0700

    Lab 1, Input and Integers

---
+++ Unit1Lab/Unit1Lab.py
@@ -0,0 +1,23 @@
+print("I Love Python")
+#myName=input()
+
+#print(myName)
+
+#print('My Name is '+myName)
+
+#print(7**2)
+#print(9.5*2)
+#print(7+9)
+#print(17%7)
+#print(97-82)
+#print(97/8)
+
+#num1=input()
+#print(num1)
+
+#num2=input()
+#print(num2)
+
+#print(num1 + num2)
+
+#print(int(num1)+int(num2))
