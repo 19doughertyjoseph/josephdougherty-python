f4ddff6614c9ccbbff1ba70e4efea3924bb89057
Gabi Hyman <19hymangabrielle@bprep.org>
2018-09-11 09:19:20 -0700

    Unit 2A Lab

---
+++ Unit2ALab/Unit2ALab.py
@@ -0,0 +1,20 @@
+print(int(2.5+4))
+print(2.5+4)
+print(float(5.2+9))
+
+
+i=100
+j=200
+x=300
+y=100
+
+
+print(i==j or i>x)
+print(i<j and i>=y)
+print(i!=j and i!=x)
+print(i<j and i<x and i<y)
+
+
+#print(i+j-y+x)
+#print(j/i)
+#print(j*x)
