eaad32f5dc0b3f3b44a3c78f317c58deb6b4b6cb
Gabi Hyman <19hymangabrielle@bprep.org>
2018-10-15 08:28:06 -0700

    Unit Lab 4B

---
+++ UnitLab4B/Unit Lab 4B.py
@@ -0,0 +1,18 @@
+for i in range(0,5):
+    print(i)
+    answer=(i+10)
+    skrt= (i*10)
+    print("pause")
+    print( str(i) + "+ 10 =" + str(answer))
+    print( str(i) + "*10 =" + str(skrt))
+    print("pause")
+
+
+myList= [10,20,30,40,50]
+print(myList)
+
+
+for a in range(0, len(myList)):
+    list=[]
+    myList[a]=myList[a] *10
+    print(myList)
